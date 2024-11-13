c4fde16271e52bf422e44fde637eeb3930af6aee
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2024-11-13 16:44:00 +0300

    fix:height issue feat:add active to nav link

--- javascript.js
+++ javascript.js
@@ -2,8 +2,15 @@ window.onload=()=>{
     var navs=document.querySelectorAll(".nav-link")
         navs.forEach((item)=>{
             item.addEventListener("click",()=>{
-                    item.style.color="red"
+
+                  navs.forEach((items)=> items.removeAttribute("id"))
+
+
+                    item.setAttribute("id","active")
             })
         })
+
+
+        
 
 }
